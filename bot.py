e2d5a3d2b15dc7c72dfebb2d29d09d5f057160db
Artem Golynin <a.golynin@yandex.ru>
2019-03-12 00:39:20 +0300

    Bot + ephem

--- bot.py
+++ bot.py
@@ -14,6 +14,21 @@ def greet_user(bot, update):
     logging.info(text)
     update.message.reply_text(text)
 
+def learn_constellation(bot, update):
+    get_planet = update.message.text.split()[1]
+        
+    import ephem
+
+    if get_planet == 'Mars':
+        planet = ephem.Mars('2019/09/12')
+        constellation = 'Планета находится в созвездии {}'.format(ephem.constellation(planet))
+        update.message.reply_text(constellation)
+    else:
+        error_text = 'Может все-таки Mars???'
+        update.message.reply_text(error_text) 
+   
+
+
 def talk_to_me(bot, update):
     user_text = "Привет, {}! Ты написал: {}".format(update.message.chat.first_name, update.message.text) 
     logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
@@ -29,6 +44,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', learn_constellation))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
